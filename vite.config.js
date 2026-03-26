5dfa82fc71752932fd8e9639d75eb99291610296
bhanuu7 <bhanuthanneeru411@gmail.com>
2026-03-26 19:34:22 +0530

    feat: initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
+import path from "path"; // Import path
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // This maps the "@" symbol to your "src" folder
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
