9aa3a0be77d38020edbbeb7a24fbb5b9659ace9d
bhanuu7 <bhanuthanneeru411@gmail.com>
2026-04-10 20:46:09 +0530

    env changes

---
+++ env.js
@@ -0,0 +1,5 @@
+VITE_BASE_URL=https://18-60-216-62.nip.io
+#http://localhost:3000
+VITE_COGNITO_USER_POOL_ID=ap-south-2_VhBfNcTOq
+VITE_COGNITO_CLIENT_ID=tliomuqr9a2t32iqtcvr0o0k
+VITE_COGNITO_REGION=ap-south-2
